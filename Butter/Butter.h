23b1c525f993ed012daf236ae2eb12363d08fa80
Jonathan Willing <jwilling@me.com>
2012-12-07 21:25:37 -0600

    improve directory structure

---
+++ Butter/Butter.h
@@ -0,0 +1,12 @@
+//
+//  ButterKit.h
+//  Butter
+//
+//  Created by Indragie Karunaratne on 2012-12-07.
+//  Copyright (c) 2012 ButterKit. All rights reserved.
+//
+
+#import <Butter/BTRCollectionView.h>
+#import <Butter/BTRCollectionViewCell.h>
+#import <Butter/BTRCollectionViewLayout.h>
+#import <Butter/BTRCollectionViewFlowLayout.h>
