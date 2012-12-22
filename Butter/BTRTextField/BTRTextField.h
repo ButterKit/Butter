433123c2e650c46e24cfd8929bd0f1b184f464f1
Jonathan Willing <jwilling@me.com>
2012-12-22 12:32:06 -0600

    add BTRTextField stub

---
+++ Butter/BTRTextField/BTRTextField.h
@@ -0,0 +1,13 @@
+//
+//  BTRTextField.h
+//  Butter
+//
+//  Created by Jonathan Willing on 12/21/12.
+//  Copyright (c) 2012 ButterKit. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface BTRTextField : NSTextField
+
+@end
