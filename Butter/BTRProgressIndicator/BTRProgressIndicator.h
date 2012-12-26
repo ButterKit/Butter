528f81c8daf839b352d2e8f8dd1a8eb314b19f46
Jonathan Willing <jwilling@me.com>
2012-12-25 21:05:20 -0500

    some work on the progress indicator

--- Butter/BTRProgressIndicator/BTRProgressIndicator.h
+++ Butter/BTRProgressIndicator/BTRProgressIndicator.h
@@ -4,9 +4,31 @@
 //
 //  Created by Jonathan Willing on 12/25/12.
 //  Copyright (c) 2012 ButterKit. All rights reserved.
+//
 
+typedef NS_ENUM(NSInteger, BTRActivityIndicatorStyle) {
+    BTRActivityIndicatorStyleWhite,
+    BTRActivityIndicatorStyleGray
+};
+
+// An indeterminate activity indicator.
+// The API is nearly equivalent to UIActivityIndicator.
 #import <Butter/Butter.h>
 
 @interface BTRProgressIndicator : BTRView
+
+// Returns an activity indicator sized to the default indicator size.
+- (id)initWithActivityIndicatorStyle:(BTRActivityIndicatorStyle)style;
+
+// Initializes the activity indicator with the default indicator style (BTRActivityIndicatorStyleGray).
+- (id)initWithFrame:(NSRect)frameRect;
+
+- (id)initWithFrame:(NSRect)frameRect activityIndicatorStyle:(BTRActivityIndicatorStyle)style;
+
+- (void)startAnimating;
+- (void)stopAnimating;
+
+@property (nonatomic, assign) BOOL hidesWhenStopped;
+@property (nonatomic, assign) BTRActivityIndicatorStyle activityIndicatorStyle;
 
 @end
--- Butter/BTRProgressIndicator/BTRProgressIndicator.m
+++ Butter/BTRProgressIndicator/BTRProgressIndicator.m
@@ -1,27 +1,41 @@
 //
-//  BTRProgressIndicator.h
+//  BTRProgressIndicator.m
 //  Butter
 //
 //  Created by Jonathan Willing on 12/25/12.
 //  Copyright (c) 2012 ButterKit. All rights reserved.
+//
 
 #import "BTRProgressIndicator.h"
 
+static const CGFloat BTRProgressIndicatorDefaultFrameLength = 14.f;
+
 @implementation BTRProgressIndicator
 
-- (id)initWithFrame:(NSRect)frame
-{
-    self = [super initWithFrame:frame];
-    if (self) {
-        // Initialization code here.
-    }
-    
-    return self;
+- (id)initWithFrame:(NSRect)frame {
+	return [self initWithFrame:frame activityIndicatorStyle:BTRActivityIndicatorStyleGray];
 }
 
-- (void)drawRect:(NSRect)dirtyRect
-{
-    // Drawing code here.
+- (id)initWithActivityIndicatorStyle:(BTRActivityIndicatorStyle)style {
+	CGFloat length = BTRProgressIndicatorDefaultFrameLength;
+	return [self initWithFrame:CGRectMake(0, 0, length, length) activityIndicatorStyle:style];
+}
+
+- (id)initWithFrame:(NSRect)frame activityIndicatorStyle:(BTRActivityIndicatorStyle)style {
+	self = [super initWithFrame:frame layerHosted:YES];
+	if (self == nil) return nil;
+	_activityIndicatorStyle = style;
+	_hidesWhenStopped = YES;
+	
+	return self;
+}
+
+- (void)startAnimating {
+	
+}
+
+- (void)stopAnimating {
+	
 }
 
 @end
