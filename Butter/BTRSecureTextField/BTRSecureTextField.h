ac5a14369145b801225e4b0ec8a1481ff2646ae5
Indragie Karunaratne <i@indragie.com>
2012-12-28 16:48:53 -0700

    Copy the changes to BTRSecureTextField (we need to find a better way to do this!)

--- Butter/BTRSecureTextField/BTRSecureTextField.h
+++ Butter/BTRSecureTextField/BTRSecureTextField.h
@@ -7,10 +7,24 @@
 //
 
 #import <Cocoa/Cocoa.h>
+#import "BTRControl.h"
+#import "BTRImageView.h"
 
 // BTRSecureTextField should *not* be layer backed in Interface Builder
 // This leads to a bug that causes an additional shadow to be shown
 // underneath the field
 @interface BTRSecureTextField : NSSecureTextField
+- (NSImage *)backgroundImageForControlState:(BTRControlState)state;
+- (void)setBackgroundImage:(NSImage *)image forControlState:(BTRControlState)state;
 
+// Modifies the contentMode on the underlying image view.
+@property (nonatomic, assign) BTRViewContentMode contentMode;
+@property (nonatomic, assign) BOOL animatesContents;
+
+// Reimplementation of BTRControl API
+@property (nonatomic, readonly) BTRControlState state;
+@property (nonatomic, getter = isHighlighted) BOOL highlighted;
+@property (nonatomic, readonly) NSInteger clickCount;
+
+- (void)addBlock:(void (^)(BTRControlEvents events))block forControlEvents:(BTRControlEvents)events;
 @end
--- Butter/BTRSecureTextField/BTRSecureTextField.m
+++ Butter/BTRSecureTextField/BTRSecureTextField.m
@@ -11,6 +11,15 @@
 
 @interface BTRSecureTextField()
 @property (nonatomic, readonly, getter = isFirstResponder) BOOL firstResponder;
+@property (nonatomic, strong) BTRImageView *backgroundImageView;
+@property (nonatomic, strong) NSMutableDictionary *backgroundImages;
+@property (nonatomic, strong) NSMutableArray *actions;
+@property (nonatomic) BOOL mouseInside;
+@property (nonatomic) BOOL mouseDown;
+@property (nonatomic) BOOL mouseHover;
+@property (nonatomic, readwrite) NSInteger clickCount;
+@property (nonatomic, strong) NSTrackingArea *trackingArea;
+@property (nonatomic, assign) BOOL needsTrackingArea;
 @end
 
 @interface BTRSecureTextFieldCell : NSSecureTextFieldCell
@@ -25,6 +34,7 @@ static CGFloat const BTRTextFieldXInset = 2.f;
 #define BTRTextFieldInactiveGradientStartingColor [NSColor colorWithDeviceWhite:0.6 alpha:1.0]
 #define BTRTextFieldInactiveGradientEndingColor [NSColor colorWithDeviceWhite:0.7 alpha:1.0]
 #define BTRTextFieldFillColor [NSColor whiteColor]
+#define BTRTextFieldShadowColor [NSColor colorWithDeviceRed:0.19 green:0.51 blue:0.81 alpha:1.0]
 
 @implementation BTRSecureTextField {
 	BOOL _btrDrawsBackground;
@@ -67,6 +77,9 @@ static CGFloat const BTRTextFieldXInset = 2.f;
 	[newCell setEditable:[oldCell isEditable]];
 	[newCell setSelectable:[oldCell isSelectable]];
 	self.cell = newCell;
+	self.backgroundImages = [NSMutableDictionary dictionary];
+	self.actions = [NSMutableArray array];
+	self.needsTrackingArea = NO;
 	
 	self.focusRingType = NSFocusRingTypeNone;
 	super.drawsBackground = NO;
@@ -74,7 +87,7 @@ static CGFloat const BTRTextFieldXInset = 2.f;
 	self.bezeled = NO;
 	
 	// Set up the layer styles used to draw a focus ring.
-	self.layer.shadowColor = [NSColor colorWithCalibratedRed:0.176 green:0.490 blue:0.898 alpha:1].CGColor;
+	self.layer.shadowColor = BTRTextFieldShadowColor.CGColor;
 	self.layer.shadowOffset = CGSizeZero;
 	self.layer.shadowRadius = 2.f;
 }
@@ -94,24 +107,51 @@ static CGFloat const BTRTextFieldXInset = 2.f;
 	[self setNeedsDisplay:YES];
 }
 
-+ (Class)cellClass
-{
++ (Class)cellClass {
 	return [BTRSecureTextFieldCell class];
 }
 
 #pragma mark - Accessors
 
-- (void)setDrawsBackground:(BOOL)flag
-{
+- (void)setDrawsBackground:(BOOL)flag {
 	if (_btrDrawsBackground != flag) {
 		_btrDrawsBackground = flag;
 		[self setNeedsDisplay:YES];
 	}
 }
 
-- (BOOL)drawsBackground
-{
+- (BOOL)drawsBackground {
 	return _btrDrawsBackground;
+}
+
+- (void)setCornerRadius:(CGFloat)cornerRadius {
+	self.backgroundImageView.cornerRadius = cornerRadius;
+}
+
+- (CGFloat)cornerRadius {
+	return self.backgroundImageView.cornerRadius;
+}
+
+- (BTRControlState)state {
+	BTRControlState state = BTRControlStateNormal;
+	if (self.highlighted) state |= BTRControlStateHighlighted;
+	if (![self isEnabled]) state |= BTRControlStateDisabled;
+	if (self.mouseHover && !self.highlighted) state |= BTRControlStateHover;
+	return state;
+}
+
+- (BTRImageView *)backgroundImageView {
+	if (!_backgroundImageView) {
+		_backgroundImageView = [[BTRImageView alloc] initWithFrame:self.bounds];
+		[self addSubview:_backgroundImageView];
+	}
+	return _backgroundImageView;
+}
+
+- (void)setEnabled:(BOOL)enabled
+{
+	[super setEnabled:enabled];
+	[self handleStateChange];
 }
 
 #pragma mark Drawing
@@ -123,15 +163,19 @@ static CGFloat const BTRTextFieldXInset = 2.f;
 	}
 	[BTRTextFieldBorderColor set];
 	if (![self isFirstResponder])
-		[[NSBezierPath bezierPathWithRoundedRect:self.bounds xRadius:BTRTextFieldCornerRadius yRadius:BTRTextFieldCornerRadius] fill];
+		[[NSBezierPath bezierPathWithRoundedRect:self.bounds
+										 xRadius:BTRTextFieldCornerRadius
+										 yRadius:BTRTextFieldCornerRadius] fill];
 	
 	NSGradient *gradient = nil;
 	CGRect borderRect = self.bounds;
 	borderRect.size.height -= 1, borderRect.origin.y += 1;
 	if([self isFirstResponder]) {
-		gradient = [[NSGradient alloc] initWithStartingColor:BTRTextFieldActiveGradientStartingColor endingColor:BTRTextFieldActiveGradientStartingColor];
+		gradient = [[NSGradient alloc] initWithStartingColor:BTRTextFieldActiveGradientStartingColor
+												 endingColor:BTRTextFieldActiveGradientStartingColor];
 	} else {
-		gradient = [[NSGradient alloc] initWithStartingColor:BTRTextFieldInactiveGradientStartingColor endingColor:BTRTextFieldInactiveGradientEndingColor];
+		gradient = [[NSGradient alloc] initWithStartingColor:BTRTextFieldInactiveGradientStartingColor
+												 endingColor:BTRTextFieldInactiveGradientEndingColor];
 	}
 	[gradient drawInBezierPath:[NSBezierPath bezierPathWithRoundedRect:borderRect xRadius:BTRTextFieldCornerRadius yRadius:BTRTextFieldCornerRadius] angle:-90];
 	
@@ -150,6 +194,171 @@ static CGFloat const BTRTextFieldXInset = 2.f;
 	return fade;
 }
 
+#pragma mark - Public API
+
+- (NSImage *)backgroundImageForControlState:(BTRControlState)state {
+	return _backgroundImages[@(state)];
+}
+
+- (void)setBackgroundImage:(NSImage *)image forControlState:(BTRControlState)state {
+	_backgroundImages[@(state)] = image;
+	[self updateState];
+}
+
+- (void)updateState {
+	NSImage *backgroundImage = [self backgroundImageForControlState:self.state];
+	if (backgroundImage == nil) {
+		backgroundImage = [self backgroundImageForControlState:BTRControlStateNormal];
+	}
+	self.drawsBackground = (backgroundImage == nil);
+	self.backgroundImageView.image = backgroundImage;
+}
+
+#pragma mark - BTRControl
+
+- (void)updateTrackingAreas {
+	if (!self.needsTrackingArea)
+		return;
+	
+	if (self.trackingArea) {
+		[self removeTrackingArea:self.trackingArea];
+		self.trackingArea = nil;
+	}
+	
+	// TODO: Figure out correct tracking to implement mouse drag
+	NSUInteger options = (NSTrackingMouseEnteredAndExited | NSTrackingActiveAlways | NSTrackingEnabledDuringMouseDrag);
+	self.trackingArea = [[NSTrackingArea alloc] initWithRect:self.bounds
+													 options:options
+													   owner:self userInfo:nil];
+	[self addTrackingArea:self.trackingArea];
+}
+
+- (void)setMouseHover:(BOOL)mouseHover {
+	[self updateStateWithOld:&_mouseHover new:mouseHover];
+}
+
+- (void)setHighlighted:(BOOL)highlighted {
+	[self updateStateWithOld:&_highlighted new:highlighted];
+}
+
+- (void)updateStateWithOld:(BOOL *)old new:(BOOL)new {
+	BOOL o = *old;
+	*old = new;
+	if (o != new) {
+		[self handleStateChange];
+	}
+}
+
+- (void)handleStateChange {
+	[self updateState];
+}
+
+- (void)addBlock:(void (^)(BTRControlEvents))block forControlEvents:(BTRControlEvents)events {
+	NSParameterAssert(block);
+	BTRControlAction *action = [BTRControlAction new];
+	action.block = block;
+	action.events = events;
+	[self.actions addObject:action];
+	[self handleUpdatedEvents:events];
+}
+
+- (void)handleUpdatedEvents:(BTRControlEvents)events {
+	// TODO: Verify if we need a tracking area here.
+	self.needsTrackingArea = YES;
+}
+
+- (void)setNeedsTrackingArea:(BOOL)needsTrackingArea {
+	_needsTrackingArea = needsTrackingArea;
+	if (!needsTrackingArea && self.trackingArea != nil) {
+		[self removeTrackingArea:self.trackingArea];
+		self.trackingArea = nil;
+	}
+}
+
+- (void)mouseDown:(NSEvent *)event {
+	[super mouseDown:event];
+	[self handleMouseDown:event];
+}
+
+- (void)mouseUp:(NSEvent *)event {
+	[super mouseUp:event];
+	[self handleMouseUp:event];
+}
+
+- (void)rightMouseUp:(NSEvent *)event {
+	[super rightMouseUp:event];
+	[self handleMouseUp:event];
+}
+
+- (void)rightMouseDown:(NSEvent *)event {
+	[super rightMouseDown:event];
+	[self handleMouseDown:event];
+}
+
+- (void)mouseEntered:(NSEvent *)event {
+	[super mouseEntered:event];
+	self.mouseInside = YES;
+	self.mouseHover = YES;
+	[self sendActionsForControlEvents:BTRControlEventMouseEntered];
+	if (self.mouseDown)
+		self.highlighted = YES;
+}
+
+- (void)mouseExited:(NSEvent *)event {
+	[super mouseExited:event];
+	self.mouseInside = NO;
+	self.mouseHover = NO;
+	[self sendActionsForControlEvents:BTRControlEventMouseExited];
+	self.highlighted = NO;
+}
+
+- (void)handleMouseDown:(NSEvent *)event {
+	self.clickCount = event.clickCount;
+	self.mouseDown = YES;
+	
+	BTRControlEvents events = 1;
+	events |= BTRControlEventMouseDownInside;
+	
+	[self sendActionsForControlEvents:events];
+	
+	self.highlighted = YES;
+}
+
+- (void)handleMouseUp:(NSEvent *)event {
+	self.mouseDown = NO;
+	
+	BTRControlEvents events = 1;
+	if (self.clickCount > 1) {
+		events |= BTRControlEventClickRepeat;
+	}
+	if (event.type == NSLeftMouseUp && self.mouseInside) {
+		events |= BTRControlEventLeftClick;
+	} else if (event.type == NSRightMouseUp && self.mouseInside) {
+		events |= BTRControlEventRightClick;
+	}
+	if (self.mouseInside) {
+		events |= BTRControlEventMouseUpInside;
+		events |= BTRControlEventClick;
+	} else {
+		events |= BTRControlEventMouseUpOutside;
+	}
+	
+	[self sendActionsForControlEvents:events];
+	
+	self.highlighted = NO;
+}
+
+- (void)sendActionsForControlEvents:(BTRControlEvents)events {
+	for (BTRControlAction *action in self.actions) {
+		if (action.events & events) {
+			if (action.block != nil) {
+				action.block(events);
+			} else if (action.action != nil) { // the target can be nil
+				[NSApp sendAction:action.action to:action.target];
+			}
+		}
+	}
+}
 
 #pragma mark Responders
 
@@ -161,15 +370,18 @@ static CGFloat const BTRTextFieldXInset = 2.f;
 - (BOOL)becomeFirstResponder {
 	[self.layer addAnimation:[self shadowOpacityAnimation] forKey:nil];
 	self.layer.shadowOpacity = 1.f;
+	self.backgroundImageView.animatesContents = NO;
+	self.highlighted = YES;
 	return [super becomeFirstResponder];
 }
 
 - (void)textDidEndEditing:(NSNotification *)notification {
 	[self.layer addAnimation:[self shadowOpacityAnimation] forKey:nil];
 	self.layer.shadowOpacity = 0.f;
+	self.backgroundImageView.animatesContents = self.animatesContents;
 	[super textDidEndEditing:notification];
+	self.highlighted = NO;
 }
-
 @end
 
 // Originally written by Daniel Jalkut as RSVerticallyCenteredTextFieldCell
