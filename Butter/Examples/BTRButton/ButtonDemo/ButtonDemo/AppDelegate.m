d0721b5fdca8b5f6dcdcc052ed4315aaf9a7591d
Jonathan Willing <jwilling@me.com>
2013-01-25 00:16:05 -0600

    update demo to reflect button changes

--- Butter/Examples/BTRButton/ButtonDemo/ButtonDemo/AppDelegate.m
+++ Butter/Examples/BTRButton/ButtonDemo/ButtonDemo/AppDelegate.m
@@ -14,6 +14,18 @@
 	[self.button setTitle:@"Standard" forControlState:BTRControlStateNormal];
 	[self.button setTitle:@"Highlighted" forControlState:BTRControlStateHighlighted];
 	
+	[self.button setBackgroundImage:[NSImage imageWithSize:CGSizeMake(1, 1) flipped:NO drawingHandler:^BOOL(NSRect dstRect) {
+		[[NSColor greenColor] set];
+		NSRectFill(dstRect);
+		return YES;
+	}] forControlState:BTRControlStateNormal];
+	
+	[self.button setBackgroundImage:[NSImage imageWithSize:CGSizeMake(1, 1) flipped:NO drawingHandler:^BOOL(NSRect dstRect) {
+		[[NSColor redColor] set];
+		NSRectFill(dstRect);
+		return YES;
+	}] forControlState:BTRControlStateHighlighted];
+	
 	self.button.animatesContents = YES;
 }
 
