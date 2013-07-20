996076c61e83009678deae3edf20df2c46740b82
Zachary Waldowski <zwaldowski@gmail.com>
2013-07-20 17:39:30 -0400

    Adds scroll view test project.

---
+++ Butter/Examples/BTRScrollView/ScrollView-Test/ScrollView-Test/BTRAppDelegate.m
@@ -0,0 +1,36 @@
+//
+//  BTRAppDelegate.m
+//  ScrollView-Test
+//
+//  Created by Zach Waldowski on 7/20/13.
+//  Copyright (c) 2013 ButterKit. All rights reserved.
+//
+
+#import "BTRAppDelegate.h"
+#import "BTRNameGenerator.h"
+
+#define ARC4RANDOM_MAX 0x100000000LL
+#define RANDF(min,max) (((double)arc4random() / ARC4RANDOM_MAX) * (max-min) + min)
+
+@implementation BTRAppDelegate
+
+- (NSDate *)randomDate
+{
+	NSTimeInterval randomInterval = RANDF(0, [NSDate timeIntervalSinceReferenceDate]);
+	return [NSDate dateWithTimeIntervalSinceReferenceDate:randomInterval];
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+	NSMutableArray *array = [NSMutableArray arrayWithCapacity:10000];
+	for (NSUInteger i = 0; i < 10000; i++) {
+		[array addObject:@{
+			@"name": [[BTRNameGenerator sharedGenerator] getName],
+			@"date": [self randomDate]
+		}];
+	}
+	self.objects = array;
+	// Insert code here to initialize your application
+}
+
+@end
