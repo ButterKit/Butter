86edb813c1fdd3872de5fd089fd279027b2003d8
Aron Cedercrantz <aron@cedercrantz.se>
2014-04-21 14:53:29 +0200

    Sync BTRSecureTextField implementation with BTRTextField.
    - They were quite a lot out of sync.
    - Both now use a shared protocol to define the shared public API. This
    should hopefully make it somewhat easier to spot if this happens in the
    future.

--- Butter/BTRSecureTextField.h
+++ Butter/BTRSecureTextField.h
@@ -7,24 +7,19 @@
 //
 
 #import <Cocoa/Cocoa.h>
-#import "BTRControl.h"
-#import "BTRImageView.h"
+#import "BTRTextFieldProtocol.h"
 
 // The secure variant of BTRTextField. See BTRTextField header for more information.
-@interface BTRSecureTextField : NSSecureTextField
+@interface BTRSecureTextField : NSSecureTextField <BTRTextField>
+@end
 
-- (NSImage *)backgroundImageForControlState:(BTRControlState)state;
-- (void)setBackgroundImage:(NSImage *)image forControlState:(BTRControlState)state;
+// Subclassing hooks for a BTRSecureTextField
+@interface BTRSecureTextField (SubclassingHooks)
 
-// Modifies the contentMode on the underlying image view.
-@property (nonatomic, assign) BTRViewContentMode contentMode;
-@property (nonatomic, assign) BOOL animatesContents;
-
-// Reimplementation of BTRControl API
-@property (nonatomic, readonly) BTRControlState state;
-@property (nonatomic, getter = isHighlighted) BOOL highlighted;
-@property (nonatomic, readonly) NSInteger clickCount;
-
-- (void)addBlock:(void (^)(BTRControlEvents events))block forControlEvents:(BTRControlEvents)events;
+- (void)drawBackgroundInRect:(NSRect)rect;
+- (NSRect)drawingRectForProposedDrawingRect:(NSRect)rect;
+- (NSRect)editingRectForProposedEditingRect:(NSRect)rect;
+- (void)setFieldEditorAttributes:(NSTextView *)fieldEditor;
 
 @end
+
